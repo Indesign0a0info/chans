c191d223a5bedc961037be5d4fa3bb5f569bca0f
project-infoprod <kyo.amisaiko@protonmail.com>
2026-02-26 23:20:07 +0100

    Turbopack behöver en absolut sökväg. Använder nu path.resolve()

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 //// next.config.ts  ← BARA i gh-pages-export-grenen
-import type { NextConfig } from 'next'
+//import type { NextConfig } from 'next'
 
 // next.config.ts
 //const isProd = process.env.NODE_ENV === 'production';
@@ -33,24 +33,43 @@ import type { NextConfig } from 'next'
 
 
 
+//const nextConfig: NextConfig = {
+//  output: 'export',           // Aktiverar statisk export
+//  basePath: '/chans',     // Byt ut REPO_NAMN mot GitHub-reponamn
+//  assetPrefix: '/chans/', // Samma som basePath
+//  trailingSlash: true,        // Rekommenderat för GitHub Pages
+//  images: {
+//    unoptimized: true,        // Krävs för statisk export
+//  },
+//  typescript: {
+//    ignoreBuildErrors: true,  // ← tillfälligt
+//  },
+//  reactStrictMode: true,
+//  turbopack: {
+//    root: '.',  // ← lägg till denna
+//  },
+//}
+//
+//export default nextConfig
+
+
+
+import type { NextConfig } from 'next'
+import path from 'path'
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  output: 'export',           // Aktiverar statisk export
-  basePath: '/chans',     // Byt ut REPO_NAMN mot GitHub-reponamn
-  assetPrefix: '/chans/', // Samma som basePath
-  trailingSlash: true,        // Rekommenderat för GitHub Pages
-  images: {
-    unoptimized: true,        // Krävs för statisk export
-  },
-  typescript: {
-    ignoreBuildErrors: true,  // ← tillfälligt
-  },
+  output: 'export',
+  basePath: isProd ? '/chans' : '',
+  assetPrefix: isProd ? '/chans/' : '',
+  trailingSlash: true,
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
   reactStrictMode: true,
   turbopack: {
-    root: '.',  // ← lägg till denna
+    root: path.resolve(__dirname),
   },
 }
 
 export default nextConfig
-
-
-
