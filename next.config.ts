3a0b055f90e800a87237f7282f9ba69d6c913798
project-infoprod <kyo.amisaiko@protonmail.com>
2026-02-26 18:26:20 +0100

    Vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // Inga basePath eller output-inställningar här
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
